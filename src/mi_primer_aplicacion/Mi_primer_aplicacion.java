12226e5e0fa098b8ea2f0a515f07089f5c152a1c
3L14S <maestrielias@gmail.com>
2021-11-01 19:12:55 -0300

    primer commit

---
+++ src/mi_primer_aplicacion/Mi_primer_aplicacion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mi_primer_aplicacion;
+
+/**
+ *
+ * @author elias
+ */
+public class Mi_primer_aplicacion {
+    public int numero;
+    public String nombre;
+    public float porcentaje;
+    public boolean llueve;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.println("hola esta es mi primera aplicacion");
+    }
+    
+}
